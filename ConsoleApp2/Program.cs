51e310f5c8a195a1c4c14626555e15422e236bf4
The Waggle <artuoh76@gmail.com>
2023-02-17 16:16:18 +0100

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("enter line: ");
+string s = Console.ReadLine();
+Console.WriteLine($"Line before sort: {s}");
+bool b = true;
+while (b)
+{
+    if (s.Contains("de"))
+    {
+        s = s.Replace("de", "ed");
+    }
+    else if (s.Contains("fd"))
+    {
+        s = s.Replace("fd", "df");
+    }
+    else if (s.Contains("gd"))
+    {
+        s = s.Replace("gd", "dg");
+    }
+    else if (s.Contains("fe"))
+    {
+        s = s.Replace("fe", "ef");
+    }
+    else if (s.Contains("ge"))
+    {
+        s = s.Replace("ge", "eg");
+    }
+    else if (s.Contains("gf"))
+    {
+        s = s.Replace("gf", "fg");
+    }
+    else
+        b=false;
+}
+Console.WriteLine($"Line afterx sort: {s}");
